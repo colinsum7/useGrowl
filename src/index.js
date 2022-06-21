c05cd7c7272aac21836c626d684be3c0cc197c03
Colin Sum <colinsum@Colins-Air.hub>
2022-06-21 19:04:44 +1200

    added useGrowl Hook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react'
-import styles from './styles.module.css'
+import './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Growl = ({ active, message, onDismissed }) => (
+  <div className={`growl${active ? " active" : ""}`}>
+    {message}
+    <div onClick={onDismissed} className="growl-close" />
+  </div>
+)
+
+export function useGrowl(dismissIn = 3) {
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false)
+
+
+  return [
+    // the first arg is the state
+    growlActive,
+
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active)
+      setTimeout(() => setGrowlActive(false), dismissIn * 1000)
+    },
+  ]
 }
